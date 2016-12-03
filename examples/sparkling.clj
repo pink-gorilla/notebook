10c6a378d72455bd7f9bddf2f6810b773f97461f
Andreas Steffan <a.steffan@contentreich.de>
2016-12-03 15:06:48 +0100

    - typo fixed

--- examples/sparkling.clj
+++ examples/sparkling.clj
@@ -2,11 +2,11 @@
 
 ;; **
 ;;; # Gorilla REPL
-;;; 
+;;;
 ;;; Welcome to gorilla :-)
-;;; 
+;;;
 ;;; Shift + enter evaluates code. Hit alt+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
-;;; 
+;;;
 ;;; It's a good habit to run each worksheet in its own namespace: feel free to use the declaration we've provided below if you'd like.
 ;; **
 
@@ -36,7 +36,7 @@
 ;; @@
 
 ;; @@
-(require '[sparkling.serialization :as requirered-to-have-serializer-class-read])
+(require '[sparkling.serialization :as required-to-have-serializer-class-read])
 ;; @@
 
 ;; @@
