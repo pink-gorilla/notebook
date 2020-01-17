c066ee0cb2a520fd0cbe44316d2c5710cc4d60e8
CI/CD <ci@pinkgorilla.org>
2020-01-17 13:58:35 +0000

    Release 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-notebook "0.4.2-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-notebook "0.4.2"
   :description "A rich REPL for Clojure in the notebook style."
   :url "https://github.com/pink-gorilla/gorilla-notebook"
   :scm {:name "git" :url "https://github.com/pink-gorilla/gorilla-notebook"}
