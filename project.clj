d926c840799e6672afc5133b11738f1a3b5a1975
CI/CD <ci@pinkgorilla.org>
2021-05-11 02:54:49 +0000

    Release 0.5.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/notebook "0.5.4-SNAPSHOT"
+(defproject org.pinkgorilla/notebook "0.5.4"
   :description "A clojure notebook web-app"
   :url "https://github.com/pink-gorilla/notebook"
   :license {:name "MIT"}
