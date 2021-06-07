4cbccbc6b1408b1319648c690e5f80f42067615b
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-07 15:57:19 -0500

    webly 0.3.0

--- src/pinkgorilla/notebook_ui/app/app.cljs
+++ src/pinkgorilla/notebook_ui/app/app.cljs
@@ -1,22 +1,21 @@
 (ns pinkgorilla.notebook-ui.app.app
   (:require
-   [webly.user.app.app :refer [webly-run!]]
 
    ; goldly
    [goldly.app]
-   [goldly-server.app] ; pages
+   [goldly-server.events]
+   [goldly-server.pages.about]
+   [goldly-server.pages.system]
+   [goldly-server.pages.system-list]
+
    ; explorer
    [pinkgorilla.explorer.default-config]
    ; notebook
    [pinkgorilla.notebook-ui.app.notebook]
    ; notebook app
    [pinkgorilla.notebook-ui.app.events]
-   [pinkgorilla.notebook-ui.app.routes :refer [routes-api routes-app]]
    [pinkgorilla.notebook-ui.app.pages.explorer]
    [pinkgorilla.notebook-ui.app.pages.document]
    [pinkgorilla.notebook-ui.app.pages.nrepl]
    [pinkgorilla.notebook-ui.app.pages.about]
    [pinkgorilla.notebook-ui.app.pages.goldly]))
-
-(defn ^:export start []
-  (webly-run! routes-api routes-app))
---
+++ src/pinkgorilla/notebook_ui/app/routes.clj
@@ -0,0 +1,22 @@
+(ns pinkgorilla.notebook-ui.app.routes
+  (:require
+   [pinkgorilla.explorer.default-config :as explorer]
+   [goldly.routes :as goldly]))
+
+(def routes
+  {:app (merge
+         explorer/routes-app
+         goldly/routes-app
+         {""  :ui/notebook-welcome
+          "about" :notebook/about ; stays here for testing.
+          "nrepl" :ui/nrepl
+          "goldly" :notebook/system-list ; notebook theme
+          })
+   :api
+   (merge
+    explorer/routes-api
+    goldly/routes-api
+    {"nrepl"  :ws/nrepl})})
+
+
+
